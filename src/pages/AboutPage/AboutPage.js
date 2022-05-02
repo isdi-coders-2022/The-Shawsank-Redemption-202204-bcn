98d61af095877b7cb1328c17489ffb2628fc37d8
josep andreu <josepa45@gmail.com>
2022-05-02 17:57:15 +0200

    Created About Page

---
+++ src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+const AboutPageContainer = styled.div`
+  background-color: #eef2f3;
+  color: #000;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: justify;
+  padding: 20px 50px;
+`;
+
+const AboutPage = () => {
+  return (
+    <AboutPageContainer>
+      <h2>Amazing Countries</h2>
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est suscipit
+          aliquid illum nihil sed saepe nisi vitae commodi aliquam vero enim sit
+          assumenda architecto eum sint provident ea, minima rem? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Natus quis suscipit quod
+          eaque quasi exercitationem perferendis reprehenderit nostrum? Dolor
+          sed dignissimos molestias facilis modi unde labore. Cupiditate ex
+          maiores sed. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quam tempore deleniti non aut ad. Aspernatur voluptatem maxime minus
+          recusandae facilis sequi, pariatur nesciunt qui ullam maiores
+          voluptates, quidem aliquid harum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est suscipit
+          aliquid illum nihil sed saepe nisi vitae commodi aliquam vero enim sit
+          assumenda architecto eum sint provident ea, minima rem? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Natus quis suscipit quod
+          eaque quasi exercitationem perferendis reprehenderit nostrum? Dolor
+          sed dignissimos molestias facilis modi unde labore. Cupiditate ex
+          maiores sed. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quam tempore deleniti non aut ad. Aspernatur voluptatem maxime minus
+          recusandae facilis sequi, pariatur nesciunt qui ullam maiores
+          voluptates, quidem aliquid harum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est suscipit
+          aliquid illum nihil sed saepe nisi vitae commodi aliquam vero enim sit
+          assumenda architecto eum sint provident ea, minima rem? Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Natus quis suscipit quod
+          eaque quasi exercitationem perferendis reprehenderit nostrum? Dolor
+          sed dignissimos molestias facilis modi unde labore. Cupiditate ex
+          maiores sed. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quam tempore deleniti non aut ad. Aspernatur voluptatem maxime minus
+          recusandae facilis sequi, pariatur nesciunt qui ullam maiores
+          voluptates, quidem aliquid harum.
+        </p>
+      </article>
+    </AboutPageContainer>
+  );
+};
+
+export default AboutPage;
