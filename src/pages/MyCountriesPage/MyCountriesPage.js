25b457e3a33ca4d7b62ad4b6e0cccbfd0465952f
AlbertMosella <mosella.albert@gmail.com>
2022-05-02 17:10:02 +0200

    Added MyCountries styles

---
+++ src/pages/MyCountriesPage/MyCountriesPage.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const MyCountriesPageContainer = styled.div`
+  background-color: #eef2f3;
+  color: #ffffff;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding: 20px;
+
+  div {
+    width: 300px;
+    height: 560px;
+    margin: 10px;
+    background-color: #cad4d8;
+    display: inline-block;
+    border-radius: 5px;
+  }
+`;
+
+export default MyCountriesPageContainer;
