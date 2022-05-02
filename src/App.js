c8ab99c836c21e88831f4282c04bb89ce1ef2ed5
AlbertMosella <mosella.albert@gmail.com>
2022-05-02 18:53:31 +0200

    Added router-dom to website

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { Navigate, Route, Routes } from "react-router-dom";
+import MyCountriesPage from "./pages/MyCountriesPage/MyCountriesPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-
-      <HomePage />
-
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/countries" element={<MyCountriesPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
       <Footer />
     </div>
   );
