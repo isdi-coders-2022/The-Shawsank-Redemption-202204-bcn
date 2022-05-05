ea92eaf5615d0604c12985734c93dc44c5675f5a
josep andreu <josepa45@gmail.com>
2022-05-05 15:09:08 +0200

    Created Add function to local api

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -5,6 +5,7 @@ import { loadMyCountries } from "../store/actions/ActionCreator";
 
 const useAPI = () => {
   const { countriesDispatch } = useContext(Context);
+
   const loadCountries = async () => {
     const fetchMyCountriesData = await fetch(
       "https://mycountries-api.onrender.com/countries"
@@ -14,7 +15,15 @@ const useAPI = () => {
     countriesDispatch(loadMyCountries(finalMyCountriesData));
   };
 
-  return { loadCountries };
+  const addCountry = async (country) => {
+    await fetch("https://mycountries-api.onrender.com/countries", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(country),
+    });
+  };
+  return { loadCountries, addCountry };
 };
-
 export default useAPI;
