a182e9d2d07378b2f255bcfd73732e154b4545ce
AlbertMosella <mosella.albert@gmail.com>
2022-05-08 18:26:27 +0200

    Remove duplicated code

---
+++ src/mocks/testUtils.js
@@ -0,0 +1,42 @@
+const testCountries = [
+  {
+    name: {
+      common: "Antarctica",
+      official: "Antarctica",
+    },
+    independent: false,
+    region: "Antarctic",
+    area: 14000000,
+    population: 1000,
+    timezones: [
+      "UTC-03:00",
+      "UTC+03:00",
+      "UTC+05:00",
+      "UTC+06:00",
+      "UTC+07:00",
+      "UTC+08:00",
+      "UTC+10:00",
+      "UTC+12:00",
+    ],
+    flags: {
+      svg: "https://flagcdn.com/aq.svg",
+    },
+  },
+  {
+    name: {
+      common: "Bahrain",
+      official: "Kingdom of Bahrain",
+    },
+    independent: true,
+    capital: ["Manama"],
+    region: "Asia",
+    area: 765,
+    population: 1701583,
+    timezones: ["UTC+03:00"],
+    flags: {
+      svg: "https://flagcdn.com/bh.svg",
+    },
+  },
+];
+
+export default testCountries;
